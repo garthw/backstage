e240060ad39ac575226a331d5e789fe967640614
Garth Whitten <garthwhitten@Garths-MacBook-Air.local>
2014-08-26 00:03:26 -0400

    Remove extra urlconf

--- servicetest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from servicetest import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.difference, name='difference'),
-    url(r'^difference/form/?$', views.form, name='form'),
-)
